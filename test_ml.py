4aee57ea03784608f5da0fc65ee69823dbcb50bf
James Dugeri <63891376+dugerij@users.noreply.github.com>
2023-02-15 21:30:14 +0100

    Edited test function

--- test_ml.py
+++ test_ml.py
@@ -1,8 +1,5 @@
-# content of test.py
 import pytest
-import random 
 
-random.seed(21)
 # import the is_prime function
     
 def sentence_checker(sentence):
@@ -12,6 +9,7 @@ def sentence_checker(sentence):
     share = ['share', 'give', 'sent',  'gave', 'send']
     email = ['email', 'contact', 'details', 'address', 'digits']
     query_markers = ['can', 'could', 'may', 'might', 'would', 'will']
+
 
     if any(word in sentence for word in share) and any(word in sentence for word in email):
         if any(query in sentence for query in query_markers):
@@ -30,8 +28,8 @@ def test_sentence():
         """
         test function to test for first group "student has shared"
         """
-        test_sharedlist = [
-        'I shared your email',
+        sentence_dict = {
+        'student has shared':['I shared your email',
         'I just shared your address',
         "I've sent your email address to my friend",
         "I've shared your email",
@@ -46,27 +44,17 @@ def test_sentence():
         'I shared your contact card',
         'I shared the email with my friends',
         'I have sent this email to my friends',
-        'The email has been shared with all my friends',
-        ]
-        a = random.randint(0, len(test_sharedlist))
-        data = test_sharedlist[a]
-        result = sentence_checker(data)
-        assert result == 'student has shared'
-
-def test_query():
-        """
-        test function to test for second group "Students want to know if can share"
-        """
-        test_querylist= [
+        'The email has been shared with all my friends',], 
+        'Students want to know if can share':[
         'Can I share your email address',
         'May I share your email',
         'Might I share your email',
         'Could we share your email address with my friends',
         'Can I share your email with my friend',
         'Can I send your email to my friend',
-        'Can I give your contacts with my friend?',
-        ]
-        b = random.randint(0, len(test_querylist))
-        data = test_querylist[b]
-        result = sentence_checker(data)
-        assert result == 'Students want to know if can share'
+        'Can I give your contacts with my friend?',]
+        }
+        for i, j in sentence_dict.items():
+            for k in range(len(j)):
+                result = sentence_checker(j[k])
+        assert result == i
